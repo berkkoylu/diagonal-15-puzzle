b4ae7975c35f37ca8179daf44cf9380d3ef51d90
Eray <eray19ayaz@gmail.com>
2020-12-18 23:36:31 +0300

    Revert "reformat code"
    This reverts commit 42de91f963c92bf0d2fcb1c7a49f511fc628c4af.

--- src/BFS.java
+++ src/BFS.java
@@ -1,29 +1,31 @@
 import java.util.*;
 
 public class BFS {
-    static int[][] solution = {{1, 2, 3, 4},
+    static int[][] solution = {{1,2,3,4},
             {12, 13, 14, 5},
-            {11, 0, 15, 6},
-            {10, 9, 8, 7}};
+            {11,0,15,6},
+            {10,9,8,7}};
     int[][] puzzle;
+    State state;
 
-    public static HashSet<State> visited = new HashSet<>();
+    public static HashSet<State> visited = new HashSet<State>();
     public static final Queue<State> frontier = new LinkedList<>();
 
 
-    public BFS(int[][] puzzle) {
+
+    public BFS(int[][] puzzle){
         this.puzzle = puzzle;
     }
 
-    public static void solve(State state) {
+    public static void solve(State state, int depth){
         frontier.clear();
         frontier.add(state);
         State currentState = state;
-        while (!frontier.isEmpty()) {
+        while(!frontier.isEmpty()){
             state = frontier.poll();
-            if (isSolution(state.getMatrixPuzzle())) {
+            if(isSolution(state.getMatrixPuzzle())){
                 currentState = state;
-                while (currentState != null) {
+                while(currentState != null){
                     printPuzzle(currentState.getMatrixPuzzle());
                     System.out.println("----------- cost of move is " + currentState.getCost());
                     currentState = currentState.getPreviousState();
@@ -44,27 +46,30 @@ public class BFS {
         }
     }
 
-    public static boolean isSolution(int[][] puzzle) {
-        if (Arrays.deepEquals(puzzle, solution)) {
+    public static boolean isSolution(int[][] puzzle){
+        if(Arrays.deepEquals(puzzle,solution)){
             return true;
-        } else {
+        }else{
             return false;
         }
     }
 
-    public static void addQueue(State state) {
-        if (state != null && !visited.contains(state)) {
+    public static void addQueue(State state){
+        if(state != null && !visited.contains(state)){
             frontier.add(state);
         }
     }
 
-    public static void printPuzzle(int[][] puzzle) {
+    public static void printPuzzle(int[][] puzzle){
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                System.out.printf("%d  ", puzzle[i][j]);
+                System.out.printf("%d  ",puzzle[i][j]);
             }
             System.out.println();
         }
     }
 
+    public static void expand(){
+
+    }
 }
