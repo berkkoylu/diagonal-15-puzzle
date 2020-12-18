3641fc4750444e1a59788f7214b3af87265c9da0
Eray <eray19ayaz@gmail.com>
2020-12-18 15:28:00 +0300

    dfs changed

--- src/DFS.java
+++ src/DFS.java
@@ -7,7 +7,7 @@ public class DFS {
             {10,9,8,7}};
     int[][] puzzle;
 
-    public static HashSet<State> visited = new HashSet<>();
+    public static HashSet<State> visited = new HashSet<State>();
     public static final Stack<State> frontier = new Stack<>();
 
 
@@ -16,7 +16,7 @@ public class DFS {
         this.puzzle = puzzle;
     }
 
-    public static void solve(State state, int depth){
+    public static void solve(State state){
         frontier.clear();
         frontier.push(state);
         State currentState;
@@ -36,42 +36,42 @@ public class DFS {
                 visited.add(state);
                 if(!visited.contains(Move.upAndRight(state))){
                     if(Move.up(state) != null){
-                        frontier.push(Move.up(state));
+                        frontier.push(Move.upAndRight(state));
                     }
                 }
                 if(!visited.contains(Move.upAndLeft(state))) {
                     if (Move.down(state) != null) {
-                        frontier.push(Move.down(state));
+                        frontier.push(Move.upAndLeft(state));
                     }
                 }
                 if(!visited.contains(Move.downAndRight(state))) {
                     if (Move.right(state) != null) {
-                        frontier.push(Move.right(state));
+                        frontier.push(Move.downAndRight(state));
                     }
                 }
                 if(!visited.contains(Move.downAndLeft(state))) {
                     if (Move.left(state) != null) {
-                        frontier.push(Move.left(state));
+                        frontier.push(Move.downAndLeft(state));
                     }
                 }
                 if(!visited.contains(Move.left(state))) {
                     if (Move.downAndLeft(state) != null) {
-                        frontier.push(Move.downAndLeft(state));
+                        frontier.push(Move.left(state));
                     }
                 }
                 if(!visited.contains(Move.right(state))) {
                     if (Move.downAndRight(state) != null) {
-                        frontier.push(Move.downAndRight(state));
+                        frontier.push(Move.right(state));
                     }
                 }
                 if(!visited.contains(Move.down(state))) {
                     if (Move.upAndLeft(state) != null) {
-                        frontier.push(Move.upAndLeft(state));
+                        frontier.push(Move.down(state));
                     }
                 }
                 if(!visited.contains(Move.up(state))) {
                     if (Move.upAndRight(state) != null) {
-                        frontier.push(Move.upAndRight(state));
+                        frontier.push(Move.up(state));
                     }
                 }
             }
